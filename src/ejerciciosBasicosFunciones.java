f22c1bdd0a7a34ccfd2fb2fedc1639ada1370ea8
dami <damianponte@hotmail.es>
2025-10-23 13:37:17 +0200

    ejercicio saludo completo

--- src/ejerciciosBasicosFunciones.java
+++ src/ejerciciosBasicosFunciones.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class ejerciciosBasicosFunciones {
-    public static void main(String[] args) {
-
-
+    public static void saludar(String nombreEntrada) {
+        System.out.println("bienvenido" + nombreEntrada + " a la programacion");
     }
+        public static void main(String[] args) {
+            // Saludo personalizado
+            //Crea una función estática llamada saludar() que reciba un nombre como parámetro (String)
+            //      y muestre por pantalla:
+            //"¡Hola, [nombre]! Bienvenido al servidor de programación."
+            //Desde main, pide el nombre por teclado y llama a la función.
+            Scanner lector=new Scanner(System.in);
+            System.out.println("¿cual es tu nombre?");
+            String nombre=lector.nextLine();
+            saludar(nombre);
+
+
+        }
+
+
 }
