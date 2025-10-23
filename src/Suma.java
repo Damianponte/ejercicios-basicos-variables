e09f912983a68150ba70bfd65f1f7a83ed609529
dami <damianponte@hotmail.es>
2025-10-23 13:53:02 +0200

    empezando ejercicio

---
+++ src/Suma.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Suma {
+    public static int suma(int a, int b) {
+       int resultado= a+b;
+       return resultado;
+    }
+    public static void main(String[] args) {
+        Scanner lector=new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("Ingrese el valor de a:");
+        a=lector.nextInt();
+        System.out.println("ingrese el valor de b");
+        b=lector.nextInt();
+        int resultadoSuma=suma(a,b);
+        String
+    }
+}
